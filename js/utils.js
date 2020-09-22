ad4cc7f2bdea037f6f45b8ffdf90c8ef0adce3e8
Oskar Lind <oskarlind@MBPsomthorOskar.lan>
2020-09-22 10:37:19 +0200

    Uppdaterat inför övning 29

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,3 @@
-console.log("Hello from utils.js")
-
-let count = 0, result = 0;
-while(count <= 3) {
-    result += count;
-    count++;
+const toggleMenu = () => {
+    document.querySelector('.menu').classList.toggle('hide')
 }
